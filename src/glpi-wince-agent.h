e3b95a5c227b39a073ce0c91356e6e42df9e4475
Guillaume Bougard <gbougard@teclib.com>
2016-02-16 17:51:45 +0100

    Added GLPI icon

--- src/glpi-wince-agent.h
+++ src/glpi-wince-agent.h
@@ -1,7 +1,8 @@
 
 #define VERSION			"0.3"
 
-#define IDR_MAINMENU			101
+#define IDR_MAINICON			101
+#define IDR_MAINMENU			102
 #define IDM_MENU_EXIT			201
 #define IDM_MENU_RUN			202
 #define IDM_MENU_DOINVENTORY	203
