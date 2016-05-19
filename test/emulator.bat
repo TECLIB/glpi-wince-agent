fcbb1987cd3dcb05a459032258f9c2dd710e7fe7
Guillaume Bougard <gbougard@teclib.com>
2016-05-19 12:06:25 +0200

    Prefer to use french image

--- test/emulator.bat
+++ test/emulator.bat
@@ -3,19 +3,25 @@
 set EXE="C:\Program Files (x86)\Microsoft Device Emulator\1.0\DeviceEmulator.exe"
 
 C:
-cd "\Program Files (x86)\Windows Mobile 6 SDK\PocketPC\Deviceemulation\0409"
+cd "\Program Files (x86)\Windows Mobile 6 SDK\PocketPC\Deviceemulation"
+rem Use 0409 for USA based image or 040C for FRA ones
+rem cd 0409
+cd 040C
 
 rem ARMv4 / ARMv5
 set CPU=ARMv5
 
-set BIN=PPC_USA_GSM_VGA_VR.BIN
+rem USA based image
+rem set BIN=PPC_USA_GSM_VGA_VR.BIN
+rem FRA based image
+set BIN=PPC_FRA_GSM_VGA_VR.BIN
 set XML=..\Pocket_PC_Phone_VGA\Pocket_PC_PE_VGA.xml
 set MEM=128
 set SHARE=C:\
 set VMNAME=TEST
 set VMID={7bd3f032-7ecb-4f3f-a8e2-376051dc0890}
 
-set OPTIONS=/c /memsize %MEM% /cpucore %CPU%
+set OPTIONS=/c /memsize %MEM% /cpucore %CPU% /h /language 040C
 set COMMONS=/sharedfolder %SHARE% /vmname %VMNAME% /vmid %VMID% /skin %XML%
 
 %EXE% %OPTIONS% %COMMONS% %BIN%  
